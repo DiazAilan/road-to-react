3f87bc75b74e6abf11321099f297b96999c7845f
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2024-12-31 16:05:21 +0100

    chore: basic iteration loop implemented in TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.scss'
 
-const TITLE = 'Hello React'
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+
+const arrayToRender = ['a', 'b', 'c']
 
 function App() {
   return (
     <div>
-      <h1><span className='debugger'>{TITLE}</span></h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <label htmlFor="search" className='debugger'>
+        Seach: 
+        <input id="search" type="text"/>
+      </label>
+      {arrayToRender.map(item =>
+        <p>{item}</p>
+      )}
     </div>
   )
 }
