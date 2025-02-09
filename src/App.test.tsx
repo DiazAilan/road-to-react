647657ec139cf415eca2d68779087b9625e01974
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-09 20:28:28 +0100

    test: add StoriesRemoveAction test for storiesReducer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,27 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import storiesMockup from './mockups/stories.json';
+import { storiesReducer } from './storiesReducer';
 
-describe('something truthy and falsy', () => {
-  it('true should be true', () => {
-    expect(true).toBe(true);
-  });
+const stories = storiesMockup
 
-  it('false should be false', () => {
-    expect(false).toBe(false);
-  });
+type StoriesRemoveAction = {
+  type: 'REMOVE_STORY';
+  payload: {id: number};
+};
+
+describe('storiesReducer', () => {
+  it('removes a story from stories', () => {
+    const action = { type: 'REMOVE_STORY', payload: { id: 1 } };
+    const state = { data: stories, isLoading: false, hasError: false };
+
+    const newState = storiesReducer(state, action as StoriesRemoveAction);
+
+    const expectedState = {
+      data: [stories[0], stories[2]],
+      isLoading: false,
+      hasError: false
+    };
+
+    expect(newState).toStrictEqual(expectedState);
+  })
 });
