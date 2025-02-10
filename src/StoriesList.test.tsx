9c561626cdd9416f06b5ae15a114e9e0b3314621
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-10 12:49:27 +0100

    test: add delete-button's tests to StoryItem component

--- src/StoriesList.test.tsx
+++ src/StoriesList.test.tsx
@@ -1,7 +1,7 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import storiesMockup from './mockups/stories.json';
 import { StoryItem } from "./StoriesList";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 const stories = storiesMockup;
 
@@ -16,6 +16,23 @@ describe('StoriesList', () => {
         'href',
         'https://reactjs.org/'
       );
+    });
+
+    it('renders a clickable delete button', () => {
+      render(<StoryItem story={stories[0]} onDelete={() => {}}/>);
+
+      const deleteButton = screen.getByRole('button');
+      expect(deleteButton).toBeInTheDocument();
+    });
+
+    it('clicking the delete button calls the callback handler', () => {
+      const handleDelete = vi.fn();
+      render(<StoryItem story={stories[0]} onDelete={handleDelete}/>);
+
+      const deleteButton = screen.getByRole('button');
+      fireEvent.click(deleteButton);
+      expect(handleDelete).toHaveBeenCalledWith(stories[0].objectID);
+      expect(handleDelete).toHaveBeenCalledTimes(1);
     });
 
   });
